17aecc32039300c3540fdb8df268e8f2916bb1a5
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-01-23 00:32:22 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function header() {
+    return (
+        <div className='head-bar'>
+            <div className='head-bar-1'>
+                <h3>Meme Generator</h3>
+            </div>
+            <div className='head-bar-2'>
+                <h3>React Course-3</h3>
+            </div>
+        </div>
+    )
+}
